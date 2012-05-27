7afb045bfade0a27a5ddf6fcd642178b9c88d9c1
Charl P. Botha <cpbotha@cpbotha.net>
2012-05-28 00:07:23 +0200

    Upped version to 0.3 for new release.

--- nvpy/nvpy.py
+++ nvpy/nvpy.py
@@ -192,7 +192,7 @@ class Controller:
             return None
                 
     def get_version(self):
-        return "0.2"
+        return "0.3"
     
     def main_loop(self):
         self.view.main_loop()
