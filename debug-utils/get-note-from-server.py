73e9a2859d96b9afcf75535547ec00957b2de74f
yuuki <yuuki0xff@gmail.com>
2018-11-08 16:54:23 +0900

    add debug utils

---
+++ debug-utils/get-note-from-server.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python2
+# Usage:
+#   ./get-note-from-server.py note_id
+
+import json
+import os
+import sys
+import ConfigParser
+
+import simplenote
+
+note_id = sys.argv[1]
+
+
+home = os.path.abspath(os.path.expanduser('~'))
+cfg_files = [
+	# os.path.join(app_dir, 'nvpy.cfg'),
+	os.path.join(home, 'nvpy.cfg'),
+	os.path.join(home, '.nvpy.cfg'),
+	os.path.join(home, '.nvpy'),
+	os.path.join(home, '.nvpyrc'),
+]
+
+cp = ConfigParser.SafeConfigParser()
+cp.read(cfg_files)
+user = cp.get('nvpy', 'sn_username', raw=True)
+passwd = cp.get('nvpy', 'sn_password', raw=True)
+
+sn = simplenote.Simplenote(user, passwd)
+note, status = sn.get_note(note_id)
+if status == 0:
+	print(json.dumps(note))
+else:
+	print(str(note))
