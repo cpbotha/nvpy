6ef839a5d29816df18ac5ece1e93aaf1ada37d33
yuuki <yuuki0xff@gmail.com>
2019-05-05 11:21:45 +0900

    dump all tracebacks when nvpy crashed

--- nvpy/debug.py
+++ nvpy/debug.py
@@ -2,21 +2,54 @@ import sys
 import functools
 import pdb
 import threading
+import traceback
+import logging
+import textwrap
 
 lock = threading.Lock()
 
 
+def format_all_tracebacks():
+    output = []
+    try:
+        for th in threading.enumerate():
+            frame = sys._current_frames()[th.ident]
+            stack = traceback.format_stack(frame)
+            output.append('Thread(ident={}, name={})\n'.format(th.ident, th.name))
+            output.extend(stack)
+    except Exception as e:
+        output.append('\nerror: ' + str(e) + '\n')
+    return ''.join(output)
+
+
 def wrap_buggy_function(fn):
     """ wrap_buggy_function handles any exception and start pdb in interactive mode. """
+
     @functools.wraps(fn)
     def wrapper(*args, **kwargs):
         try:
             return fn(*args, **kwargs)
         except Exception as e:
             with lock:
+                # Write debugging information to the log file and stderr.
+                msg = textwrap.dedent('''
+                    ERROR: An unexpected error occurred.
+                    Thread(ident={thread_ident}, name={thread_name})
+                    {traceback}
+                    Other threads:
+                    {all_threads}
+                ''').format(
+                    thread_ident=threading.current_thread().ident,
+                    thread_name=threading.current_thread().name,
+                    traceback=traceback.format_exc(),
+                    all_threads=format_all_tracebacks(),
+                )
+                logging.critical(msg)
+                sys.stderr.write(msg)
+
+                # Start pdb.
                 tb = sys.exc_info()[2]
                 pdb.post_mortem(tb)
                 raise
 
     return wrapper
-
