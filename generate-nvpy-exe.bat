e83c967231301dc2b1572348dbf2a2434bffeb24
yuuki <yuuki0xff@gmail.com>
2019-09-13 12:53:09 +0900

    improve start-up performance of nvpy.exe

--- generate-nvpy-exe.bat
+++ generate-nvpy-exe.bat
@@ -6,8 +6,8 @@ rem   Python 3.7
 rem   PyInstaller
 rem
 rem Output:
-rem   dist\nvpy.exe
-rem   dist\nvpy-debug.exe
+rem   dist\nvpy-windows.zip
+rem   dist\nvpy-windows-debug.zip
 
 rem Remove build cache and executable files before starting build.
 rmdir /q /s build
@@ -23,5 +23,12 @@ rem When build nvpy on the clean environment, this process is required.
 pip install --upgrade Markdown docutils simplenote
 
 python setup.py clean
-pyinstaller --onefile -i nvpy\icons\nvpy.ico --add-binary "nvpy\icons\nvpy.gif;icons" -n nvpy       --windowed start-nvpy.py
-pyinstaller --onefile -i nvpy\icons\nvpy.ico --add-binary "nvpy\icons\nvpy.gif;icons" -n nvpy-debug --console  start-nvpy.py
+
+rem Generate one-folder bundles.
+rem NOTE:
+rem   The one-file bundles have a start-up performance issue.  Therefore, the one-file bundle is no longer provided.
+pyinstaller -i nvpy\icons\nvpy.ico --add-binary "nvpy\icons\nvpy.gif;icons" -n nvpy       --windowed start-nvpy.py
+pyinstaller -i nvpy\icons\nvpy.ico --add-binary "nvpy\icons\nvpy.gif;icons" -n nvpy-debug --console  start-nvpy.py
+powershell -Command 'Compress-Archive dist/nvpy dist/nvpy-windows.zip'
+powershell -Command 'Compress-Archive dist/nvpy-debug dist/nvpy-windows-debug.zip'
+
