740b6668b34d8f1e46e65a1293a9c6f8331b18c4
yuuki <yuuki0xff@gmail.com>
2019-09-13 13:01:54 +0900

    use python lancher

--- generate-nvpy-exe.bat
+++ generate-nvpy-exe.bat
@@ -2,7 +2,7 @@ rem This batch generates an executable file for Windows.
 rem
 rem Requirements:
 rem   Windows 10
-rem   Python 3.7
+rem   Python 3.7 and Python launcher
 rem   PyInstaller
 rem
 rem Output:
@@ -15,23 +15,23 @@ del dist\nvpy.exe
 del dist\nvpy-debug.exe
 
 rem Upgrade pyinstaller.
-pip install --upgrade pyinstaller
+py -3 -m pip install --upgrade pyinstaller
 
 rem When you generate windows binary, you need the certifi package.
 rem See workaround code on nvpy.py for details.
-pip install --upgrade certifi
+py -3 -m pip install --upgrade certifi
 
 rem Install dependencies.
 rem When build nvpy on the clean environment, this process is required.
-pip install --upgrade Markdown docutils simplenote
+py -3 -m pip install --upgrade Markdown docutils simplenote
 
-python setup.py clean
+py -3 setup.py clean
 
 rem Generate one-folder bundles.
 rem NOTE:
 rem   The one-file bundles have a start-up performance issue.  Therefore, the one-file bundle is no longer provided.
-pyinstaller -i nvpy\icons\nvpy.ico --add-binary "nvpy\icons\nvpy.gif;icons" -n nvpy       --windowed start-nvpy.py
-pyinstaller -i nvpy\icons\nvpy.ico --add-binary "nvpy\icons\nvpy.gif;icons" -n nvpy-debug --console  start-nvpy.py
+py -3 -m PyInstaller -i nvpy\icons\nvpy.ico --add-binary "nvpy\icons\nvpy.gif;icons" -n nvpy       --windowed start-nvpy.py
+py -3 -m PyInstaller -i nvpy\icons\nvpy.ico --add-binary "nvpy\icons\nvpy.gif;icons" -n nvpy-debug --console  start-nvpy.py
 powershell -Command 'Compress-Archive dist/nvpy dist/nvpy-windows.zip'
 powershell -Command 'Compress-Archive dist/nvpy-debug dist/nvpy-windows-debug.zip'
 
