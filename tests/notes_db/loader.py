93991824cc967b42fa219030b39e08e85e7ee090
yuuki <yuuki0xff@gmail.com>
2020-05-26 12:47:18 +0900

    add test cases for NotesDB

---
+++ tests/notes_db/loader.py
@@ -0,0 +1,82 @@
+import os
+import shutil
+import unittest
+import pathlib
+import json
+from nvpy.nvpy import Config
+from nvpy.notes_db import NotesDB
+
+now = 1111111444
+note1 = {
+    'modifydate': 1111111222,
+    'tags': [],
+    'createdate': 1111111111,
+    'syncdate': 0,
+    'content': 'note',
+    'savedate': now,
+}
+note2 = {
+    'modifydate': 1111111333,
+    'tags': [],
+    'createdate': 1111111222,
+    'syncdate': 0,
+    'content': 'another note\nfoo bar',
+    'savedate': now,
+}
+
+
+class Loader(unittest.TestCase):
+    BASE_DIR = '/tmp/.nvpyUnitTests'
+
+    def setUp(self):
+        if os.path.isdir(self.BASE_DIR):
+            shutil.rmtree(self.BASE_DIR)
+
+    def __mock_config(self):
+        app_dir = os.path.abspath('nvpy')
+
+        mockConfig = Config(app_dir, [])
+        mockConfig.sn_username = ''
+        mockConfig.sn_password = ''
+        mockConfig.db_path = self.BASE_DIR
+        mockConfig.txt_path = self.BASE_DIR + '/notes'
+        mockConfig.simplenote_sync = 0
+
+        return mockConfig
+
+    @property
+    def __dir(self) -> pathlib.Path:
+        dir = pathlib.Path(self.BASE_DIR)
+        if not dir.is_dir():
+            dir.mkdir()
+        return dir
+
+    def __write_json(self, key: str, data):
+        fpath = self.__dir / (key + '.json')
+        with fpath.open('w') as f:
+            json.dump(data, f)
+
+    def __write_text(self, key: str, data: str):
+        fpath = self.__dir / (key + '.txt')
+        with fpath.open('w') as f:
+            f.write(data)
+
+    def test_database_can_load_from_empty_dir(self):
+        db = NotesDB(self.__mock_config())
+        self.assertDictEqual(db.notes, {})
+
+    def test_database_can_load_json_files(self):
+        self.__write_json('1', note1)
+        self.__write_json('2', note2)
+        db = NotesDB(self.__mock_config())
+        self.assertSetEqual(set(db.notes.keys()), {'1', '2'})
+
+    def test_database_can_load_from_text_files(self):
+        self.__write_json('1', note1)
+        self.__write_json('2', note2)
+        self.__write_text('note', note1['content'])
+        self.__write_text('another note', note2['content'])
+        db = NotesDB(self.__mock_config())
+        self.assertSetEqual(set(db.notes.keys()), {'1', '2'})
+        self.assertEqual(db.notes['1']['content'], note1['content'])
+        self.assertEqual(db.notes['2']['content'], note2['content'])
