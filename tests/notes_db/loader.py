1a270e1f639f24c022383af81b90ddfac4317927
yuuki <yuuki0xff@gmail.com>
2020-05-26 15:15:06 +0900

    fix that notes_as_txt option is not enabled

--- tests/notes_db/loader.py
+++ tests/notes_db/loader.py
@@ -32,7 +32,7 @@ class Loader(unittest.TestCase):
         if os.path.isdir(self.BASE_DIR):
             shutil.rmtree(self.BASE_DIR)
 
-    def __mock_config(self):
+    def __mock_config(self, notes_as_txt=False):
         app_dir = os.path.abspath('nvpy')
 
         mockConfig = Config(app_dir, [])
@@ -41,23 +41,31 @@ class Loader(unittest.TestCase):
         mockConfig.db_path = self.BASE_DIR
         mockConfig.txt_path = self.BASE_DIR + '/notes'
         mockConfig.simplenote_sync = 0
+        mockConfig.notes_as_txt = notes_as_txt
 
         return mockConfig
 
     @property
-    def __dir(self) -> pathlib.Path:
+    def __json_dir(self) -> pathlib.Path:
         dir = pathlib.Path(self.BASE_DIR)
         if not dir.is_dir():
             dir.mkdir()
         return dir
 
+    @property
+    def __text_dir(self) -> pathlib.Path:
+        dir = self.__json_dir / 'notes'
+        if not dir.is_dir():
+            dir.mkdir()
+        return dir
+
     def __write_json(self, key: str, data):
-        fpath = self.__dir / (key + '.json')
+        fpath = self.__json_dir / (key + '.json')
         with fpath.open('w') as f:
             json.dump(data, f)
 
     def __write_text(self, key: str, data: str):
-        fpath = self.__dir / (key + '.txt')
+        fpath = self.__text_dir / (key + '.txt')
         with fpath.open('w') as f:
             f.write(data)
 
@@ -75,8 +83,8 @@ class Loader(unittest.TestCase):
         self.__write_json('1', note1)
         self.__write_json('2', note2)
         self.__write_text('note', note1['content'])
-        self.__write_text('another note', note2['content'])
-        db = NotesDB(self.__mock_config())
+        self.__write_text('another_note', note2['content'])
+        db = NotesDB(self.__mock_config(notes_as_txt=True))
         self.assertSetEqual(set(db.notes.keys()), {'1', '2'})
         self.assertEqual(db.notes['1']['content'], note1['content'])
         self.assertEqual(db.notes['2']['content'], note2['content'])
