04f3f8d21dbb6f838aea6c9e0af0fda63c5fcb97
yuuki <yuuki0xff@gmail.com>
2020-09-13 18:30:06 +0900

    update validation logic for savedate field

--- debug-utils/validate.py
+++ debug-utils/validate.py
@@ -3,10 +3,12 @@ import json
 import os
 import glob
 import traceback
+import time
 
 cache_dir = os.path.expanduser('~/.nvpy')
 files = glob.glob(cache_dir + '/*.json')
 
+now = time.time()
 is_valid = True
 for file in files:
     with open(file) as f:
@@ -21,11 +23,12 @@ for file in files:
             assert 'systemtags' in obj and type(obj['systemtags']) == list
             assert 'tags' in obj and type(obj['tags']) == list
             assert 'content' in obj and type(obj['content']) == str
-            # nvpy required fields.
 
-            # TODO: enable 'savedate' field validation
-            # NOTE: temporarily ignore 'savedate' field validation.
-            # assert 'savedate' in obj   and type(obj['savedate'] in [float, int])
+            # Optional field for nvpy.
+            if 'savedate' in obj:
+                assert type(obj['savedate'] in [float, int])
+                assert float(obj['savedate']) <= now
+            # Required field for nvpy.
             assert 'syncdate' in obj and type(obj['syncdate'] in [float, int])
         except:
             print('{}  Invalid'.format(file))
