cpbotha/nvpy

commit 2fc551e8ef6cce5d809a0961fffc04eafaad5c64

prevent loading nvpy.cfg on unit test cases
Background:
Some test cases use nvpy.config.Config. Current implementation,
Config loads external settings from nvpy.cfg during unit test.
It may cause unexpected issues.

To solve the above issue, Config's constructor arguments should be
represent more than 3 states like this:

- Load configuration from default locations.
- Don't load any configuration file.
- Load configuration from specified (maybe non-default) configuration file.

Changes:
- Change Config's constructor argument type to the optional list.
- Change file loading logic in the Config class.

debug-utils/nvpy-db-utils.py
@@ -183,7 +183,7 @@ class UpdateCmd:
 
 def parse_cmd_line_args():
     p = argparse.ArgumentParser()
-    p.add_argument('--cfg', '-c', dest='cfg', metavar='nvpy.cfg', help='path to config file')
+    p.add_argument('--cfg', '-c', dest='cfg', type=pathlib.Path, metavar='nvpy.cfg', help='path to config file')
     sp = p.add_subparsers(required=True)
 
     validate = sp.add_parser('validate')
@@ -208,7 +208,10 @@ def parse_cmd_line_args():
 
 def main():
     args = parse_cmd_line_args()
-    config = nvpy.Config(nvpy.get_appdir(), args.cfg)
+    cfg_files = None
+    if args.ns:
+        cfg_files = [args.cfg]
+    config = nvpy.Config(nvpy.get_appdir(), cfg_files)
     return args.clazz().run(args, config)
 
 

nvpy/nvpy.py
@@ -76,6 +76,8 @@ except ImportError:
 else:
     HAVE_DOCUTILS = True
 
+PathList = typing.List[pathlib.Path]
+
 
 class ColorConfig(typing.NamedTuple):
     # Text color.
@@ -102,10 +104,12 @@ class Config:
     @ivar ok: True if config files had a default section, False otherwise.
     """
 
-    def __init__(self, app_dir: str, cfg: typing.Optional[str]):
+    def __init__(self, app_dir: str, cfg_files: typing.Optional[PathList] = None):
         """
         @param app_dir: the directory containing nvpy.py
-        @param cfg: path to configuration file
+        @param cfg_files: List of path to configuration files (optional)
+
+        If cfg_files is omitted, Config reads configuration files from default locations.
         """
         is_linux = platform.system() == "Linux"
 
@@ -179,7 +183,8 @@ class Config:
             'streamline_interface': 'false',
         }
 
-        self.files_read, cp = self._load_cfg(defaults, cfg)
+        normalized_cfg_files = self._list_cfg_files(cfg_files)
+        self.files_read, cp = self._load_cfg(defaults, normalized_cfg_files)
 
         cfg_sec = 'nvpy'
 
@@ -254,33 +259,10 @@ class Config:
             w = lambda: logging.warning('"background_full_sync" option is removed.')
             self.warnings.append(w)
 
-    def _load_cfg(self, defaults: dict, cfg: typing.Optional[str]) -> typing.Tuple[typing.List[str], ConfigParser]:
-        """ Load configuration files.
-        If cfg argument is specified, read only specified file. Otherwise, read configuration files from some locations.
-
-        Args:
-            defaults: Dict of default key values.
-            cfg: Path to config file (optional).
-
-        Returns:
-            Returns following elements.
-            1st element: List of config file paths.  It may be empty (list of zero length, not None).
-            2nd element: ConfigParser object.
-        """
-        cp = ConfigParser(defaults)
-        if cfg:
-            # Read either a specific path to a config file, or a path relative to the nvpy.py location. Other config
-            # files should not read.
-            cfg1 = pathlib.Path(cfg).absolute()
-            cfg2 = pathlib.Path(self.app_dir) / cfg
-            if cfg1.exists():
-                cfg_files = [cfg1]
-            elif cfg2.exists():
-                cfg_files = [cfg2]
-            else:
-                logging.error(f'Not found configuration file from {cfg1} or {cfg2}')
-                cfg_files = []
-        else:
+    def _list_cfg_files(self, cfg_files: typing.Optional[PathList]) -> PathList:
+        """ List up nvPY configuration files. """
+        if cfg_files is None:
+            # No configuration path specified. Use default locations.
             # Later config files overwrite earlier files try a number of alternatives.
             home = pathlib.Path.home()
             env_dir = os.environ.get("XDG_CONFIG_HOME")
@@ -293,6 +275,23 @@ class Config:
                 home / '.nvpyrc',
                 xdg_config_home / 'nvpy.cfg',
             ]
+
+        return cfg_files
+
+    def _load_cfg(self, defaults: dict, cfg_files: PathList) -> typing.Tuple[typing.List[str], ConfigParser]:
+        """ Load configuration files.
+        If cfg argument is specified, read only specified file. Otherwise, read configuration files from some locations.
+
+        Args:
+            defaults: Dict of default key values.
+            cfg: Path to config file.
+
+        Returns:
+            Returns following elements.
+            1st element: List of config file paths.  It may be empty (list of zero length, not None).
+            2nd element: ConfigParser object.
+        """
+        cp = ConfigParser(defaults)
         return cp.read(cfg_files), cp
 
     @property
@@ -1001,13 +1000,16 @@ def parse_cmd_line_args(args: typing.Optional[typing.List] = None) -> argparse.N
         A namespace object generated by argparse.
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument('--cfg', '-c', dest='cfg', metavar='nvpy.cfg', help='path to config file')
+    parser.add_argument('--cfg', '-c', dest='cfg', type=pathlib.Path, metavar='nvpy.cfg', help='path to config file')
     return parser.parse_args(args)
 
 
 def main(args: typing.Optional[typing.List] = None):
     ns = parse_cmd_line_args(args)
-    config = Config(get_appdir(), ns.cfg)
+    cfg_files = None
+    if ns.cfg is not None:
+        cfg_files = [ns.cfg]
+    config = Config(get_appdir(), cfg_files)
 
     try:
         controller = Controller(config)

commit 7e6c50626e9c67546523e68a16e72aca16cc0ef2

Fix incorrect field name on nvpy-db-utils
This bug was introduced by 2fc551e8ef6cce5d809a0961fffc04eafaad5c64.

debug-utils/nvpy-db-utils.py
@@ -210,7 +210,7 @@ def parse_cmd_line_args():
 def main():
     args = parse_cmd_line_args()
     cfg_files = None
-    if args.ns:
+    if args.cfg:
         cfg_files = [args.cfg]
     config = nvpy.Config(nvpy.get_appdir(), cfg_files)
     return args.clazz().run(args, config)